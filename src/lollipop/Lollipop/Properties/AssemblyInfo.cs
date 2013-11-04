7db5041b6553b91c2987a56098cfa8ba96c7e8b6
Bunk <jd.courtoy@gmail.com>
2013-11-04 11:52:43 -0600

    Added some NuGet information so that folks can use this.

--- src/lollipop/Lollipop/Properties/AssemblyInfo.cs
+++ src/lollipop/Lollipop/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Lollipop")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A League of Legends API client library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("JD Courtoy")]
 [assembly: AssemblyProduct("Lollipop")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
